7ddcf1fe2ec36c19e64432f6796b7a2a48952c3f
unknown <minjun3021@naver.com>
2019-06-21 03:21:28 +0900

    ranking item\

--- app/src/main/java/com/kmj/studify/fragment/RankingFragment.java
+++ app/src/main/java/com/kmj/studify/fragment/RankingFragment.java
@@ -43,28 +43,28 @@ public class RankingFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_ranking, container, false);
         mainActivity= (MainActivity) getActivity();
 
-        btn=v.findViewById(R.id.ranking_btn);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                NetworkHelper.getInstance().Ranking().enqueue(new Callback<ArrayList<UserModel>>() {
-                    @Override
-                    public void onResponse(Call<ArrayList<UserModel>> call, Response<ArrayList<UserModel>> response) {
-                        Log.e("rankingretrofit",response.toString());
-                        ranking=response.body();
-                        Log.e("How many",String.valueOf(ranking.size()));
-                        Log.e("1st",ranking.get(0).getName());
-                        Log.e("1st",ranking.get(0).getFacebookId());
-                    }
-
-                    @Override
-                    public void onFailure(Call<ArrayList<UserModel>> call, Throwable t) {
-                        Log.e("ohmygoderrror",t.toString());
-                    }
-                });
-
-            }
-        });
+//        btn=v.findViewById(R.id.ranking_btn);
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                NetworkHelper.getInstance().Ranking().enqueue(new Callback<ArrayList<UserModel>>() {
+//                    @Override
+//                    public void onResponse(Call<ArrayList<UserModel>> call, Response<ArrayList<UserModel>> response) {
+//                        Log.e("rankingretrofit",response.toString());
+//                        ranking=response.body();
+//                        Log.e("How many",String.valueOf(ranking.size()));
+//                        Log.e("1st",ranking.get(0).getName());
+//                        Log.e("1st",ranking.get(0).getFacebookId());
+//                    }
+//
+//                    @Override
+//                    public void onFailure(Call<ArrayList<UserModel>> call, Throwable t) {
+//                        Log.e("ohmygoderrror",t.toString());
+//                    }
+//                });
+//
+//            }
+//        });
 
 
         return v;
