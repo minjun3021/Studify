89a2ae39094f2447d5f884779ae87c908f247313
unknown <minjun3021@naver.com>
2019-06-21 04:33:24 +0900

    custombottombarfinish

--- app/src/main/java/com/kmj/studify/activity/MainActivity.java
+++ app/src/main/java/com/kmj/studify/activity/MainActivity.java
@@ -6,11 +6,14 @@ import android.content.res.ColorStateList;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.kmj.studify.FragmentUtils;
 import com.kmj.studify.R;
@@ -25,15 +28,22 @@ public class MainActivity extends AppCompatActivity {
     private TimerFragment timerFragment;
     private RankingFragment rankingFragment;
     private FriendsFragment friendsFragment;
-    private BottomNavigationView bottomNavigationView;
+    ConstraintLayout timer,friends,ranking;
     ArrayList<Fragment> fragments;
     FragmentUtils fragmentUtils;
+    ImageView ti,fi,ri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        timer=findViewById(R.id.timer);
+        friends=findViewById(R.id.friends);
+        ranking=findViewById(R.id.ranking);
+        ti=findViewById(R.id.timer_img);
+        fi=findViewById(R.id.friends_img);
+        ri=findViewById(R.id.ranking_img);
 
 
 //
@@ -44,45 +54,44 @@ public class MainActivity extends AppCompatActivity {
         timerFragment = new TimerFragment();
         rankingFragment = new RankingFragment();
         friendsFragment = new FriendsFragment();
-        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+
         fragments.add(timerFragment);
 
         fragments.add(friendsFragment);
         fragments.add(rankingFragment);
         fragmentUtils = new FragmentUtils(R.id.frame_layout, fragments);
         fragmentUtils.setCurrentFragmentByPosition(getSupportFragmentManager(), 0, new Bundle());
-        final MenuItem timer=bottomNavigationView.getMenu().getItem(0);
-        final MenuItem friends=bottomNavigationView.getMenu().getItem(1);
-        final MenuItem ranking=bottomNavigationView.getMenu().getItem(2);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        ti.setImageResource(R.drawable.on_timer);
+
+        timer.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()) {
-                    case R.id.navigation_timer:
-                        timer.setIcon(R.drawable.on_timer);
-                        friends.setIcon(R.drawable.friends);
-                        ranking.setIcon(R.drawable.ranking);
-                        fragmentUtils.setCurrentFragmentByPosition(getSupportFragmentManager(), 0, new Bundle());
-                        break;
-                    case R.id.navigation_friends:
-                        timer.setIcon(R.drawable.timer);
-                        friends.setIcon(R.drawable.on_friends);
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                            friends.setIconTintList(ColorStateList.valueOf(0xffebb42c));
-                        }
-                        ranking.setIcon(R.drawable.ranking);
-                        fragmentUtils.setCurrentFragmentByPosition(getSupportFragmentManager(), 1, new Bundle());
-                        break;
-                    case R.id.navigation_ranking:
-                        timer.setIcon(R.drawable.timer);
-                        friends.setIcon(R.drawable.friends);
-                        ranking.setIcon(R.drawable.on_ranking);
-                        fragmentUtils.setCurrentFragmentByPosition(getSupportFragmentManager(), 2, new Bundle());
-                        break;
-                }
-                return false;
+            public void onClick(View v) {
+                ti.setImageResource(R.drawable.on_timer);
+                fi.setImageResource(R.drawable.friends);
+                ri.setImageResource(R.drawable.ranking);
+                fragmentUtils.setCurrentFragmentByPosition(getSupportFragmentManager(), 0, new Bundle());
             }
         });
+
+        friends.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ti.setImageResource(R.drawable.timer);
+                fi.setImageResource(R.drawable.on_friends);
+                ri.setImageResource(R.drawable.ranking);
+                fragmentUtils.setCurrentFragmentByPosition(getSupportFragmentManager(), 1, new Bundle());
+            }
+        });
+        ranking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ti.setImageResource(R.drawable.timer);
+                fi.setImageResource(R.drawable.friends);
+                ri.setImageResource(R.drawable.on_ranking);
+                fragmentUtils.setCurrentFragmentByPosition(getSupportFragmentManager(), 2, new Bundle());
+            }
+        });
+
 
 
     }
