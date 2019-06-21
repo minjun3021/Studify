e5f8d01d2eefa9df0fe1fdab68580786539c356b
unknown <minjun3021@naver.com>
2019-06-21 17:24:42 +0900

    friendsalmost

---
+++ app/src/main/java/com/kmj/studify/FriendsAdapter.java
@@ -0,0 +1,95 @@
+package com.kmj.studify;
+
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.kmj.studify.activity.MainActivity;
+import com.kmj.studify.data.UserModel;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.FriendsViewHolder>  {
+
+    ArrayList<UserModel> mList;
+    MainActivity mainActivity;
+
+    public FriendsAdapter(ArrayList<UserModel> mList, MainActivity mainActivity) {
+        this.mList = mList;
+        this.mainActivity = mainActivity;
+    }
+
+    public class FriendsViewHolder extends RecyclerView.ViewHolder {
+        protected CircleImageView circleImageView;
+        protected TextView fname;
+        protected TextView time;
+        protected CircleImageView doing;
+
+        public FriendsViewHolder(@NonNull View v) {
+            super(v);
+            this.circleImageView=(CircleImageView) v.findViewById(R.id.friend_pic);
+            this.fname=(TextView) v.findViewById(R.id.friends_name);
+            this.time=(TextView)v.findViewById(R.id.friends_time);
+            this.doing=(CircleImageView) v.findViewById(R.id.friendDoing);
+
+        }
+    }
+    @NonNull
+    @Override
+    public FriendsViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.friend_item, viewGroup, false);
+
+        FriendsAdapter.FriendsViewHolder viewHolder = new FriendsAdapter.FriendsViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FriendsViewHolder friendsViewHolder, int i) {
+        friendsViewHolder.fname.setText(mList.get(i).getName());
+        Glide.with(mainActivity)
+                .load(mList.get(i).getProfileURL())
+                .fitCenter()
+                .into(friendsViewHolder.circleImageView);
+        if(mList.get(i).getStart_time()==-1 && mList.get(i).getEnd_time()==-1){
+            friendsViewHolder.time.setText("");
+            friendsViewHolder.doing.setBackgroundColor(Color.rgb(191, 191, 191));
+        }
+        else if(mList.get(i).getStart_time()==-1){
+            friendsViewHolder.doing.setBackgroundColor(Color.rgb(191, 191, 191));
+            friendsViewHolder.time.setText("");
+        }
+        else if(mList.get(i).getEnd_time()==-1){
+
+            long time;
+            time=System.currentTimeMillis()-mList.get(i).getStart_time();
+            Log.e("currentitme",String.valueOf(System.currentTimeMillis())+" "+String.valueOf(mList.get(i).getStart_time()));
+            Log.e("minus",String.valueOf(time));
+            time/=1000;
+            int hour, min, sec;
+            hour = (int) (time / 3600);
+            min = (int) (time% 3600 / 60);
+            sec = (int) (time% 3600 % 60);
+            String max = String.valueOf(hour) + " : " + String.valueOf(min) + " : " + String.valueOf(sec);
+            friendsViewHolder.time.setText(max);
+
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+
+
+        return (null != mList ? mList.size() : 0);
+    }
+
+
+}
--- app/src/main/java/com/kmj/studify/fragment/FriendsFragment.java
+++ app/src/main/java/com/kmj/studify/fragment/FriendsFragment.java
@@ -1,18 +1,23 @@
 package com.kmj.studify.fragment;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.facebook.AccessToken;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.HttpMethod;
+import com.kmj.studify.FriendsAdapter;
 import com.kmj.studify.NetworkHelper;
 import com.kmj.studify.R;
 import com.kmj.studify.activity.MainActivity;
@@ -24,16 +29,24 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+
+import static android.content.Context.MODE_PRIVATE;
 
 
 public class FriendsFragment extends Fragment {
     ArrayList<UserModel> friendsranking;
     MainActivity mainActivity;
-    Button btn;
-    String friendsFacebookIds="";
+    String friendsFacebookIds = "";
+    CircleImageView mypic;
+    TextView tvname;
+    Handler handler;
+    private RecyclerView mRecyclerView;
+    private LinearLayoutManager mLinearLayoutManager;
+    private FriendsAdapter friendsAdapter;
 
 
     public FriendsFragment newInstance() {
@@ -53,105 +66,115 @@ public class FriendsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_friends, container, false);
-//        mainActivity = (MainActivity) getActivity();
-//        btn = v.findViewById(R.id.friends_btn);
-//        btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                final AccessToken accessToken = AccessToken.getCurrentAccessToken();
-//                GraphRequest request = GraphRequest.newMeRequest(accessToken, new GraphRequest.GraphJSONObjectCallback() {
-//                    @Override
-//                    public void onCompleted(JSONObject object, GraphResponse response) {
-//
-//                        new GraphRequest(
-//                                accessToken,
-//                                // "/me/friends",
-//                                //"me/taggable_friends",
-//                                "/me/friends",
-//                                null,
-//                                HttpMethod.GET,
-//                                new GraphRequest.Callback() {
-//                                    public void onCompleted(GraphResponse response) {
-//
-//                                        try {
-//
-//                                            JSONArray rawName = response.getJSONObject().getJSONArray("data");
-//                                            Log.e("Json Array Length ", "Json Array Length " + rawName.length());
-//                                            Log.e("Json Array", "Json Array " + rawName.toString());
-//
-//
-//                                            for (int i = 0; i < rawName.length(); i++) {
-//                                                JSONObject c = rawName.getJSONObject(i);
-//
-//
-//                                                String name = c.getString("name");
-//                                                Log.e("Friends's Name", "JSON NAME :" + name);
-//
-//                                                String id = c.getString("id");
-//                                                Log.e("Friends's ID :", name + "'s ID:" + id);
-//                                                if(i==rawName.length()-1) {
-//                                                    friendsFacebookIds += id;
-//                                                }
-//                                                else{
-//                                                    friendsFacebookIds +=id+",";
-//                                                }
-//
-//                                            }
-//                                            Log.e("friendsFacebookIds",friendsFacebookIds);
-//
-//
-//                                        } catch (JSONException e) {
-//                                            e.printStackTrace();
-//                                            Log.e("trycatch실패",e.toString());
-//                                        }
-//
-//                                    }
-//                                }
-//                        ).executeAsync();
-//
-//
-//                    }
-//
-//
-//                });
-//
-//
-//                Bundle parameters = new Bundle();
-//                parameters.putString("fields", "id,name,profile_pic");
-//                request.setParameters(parameters);
-//                request.executeAsync();
-//                new Handler().postDelayed(new Runnable()
-//                {
-//                    @Override
-//                    public void run()
-//                    {
-//                        NetworkHelper.getInstance().FriendsRanking(friendsFacebookIds).enqueue(new Callback<ArrayList<UserModel>>() {
-//                            @Override
-//                            public void onResponse(Call<ArrayList<UserModel>> call, Response<ArrayList<UserModel>> response) {
-//                                Log.e("rankingretrofit",response.toString());
-//                                friendsranking=response.body();
-//                                Log.e("How many",String.valueOf(friendsranking.size()));
-//                                if(friendsranking.size()!=0){
-//                                    Log.e("1st",friendsranking.get(0).getName());
-//                                    Log.e("1st",friendsranking.get(0).getFacebookId());
-//                                }
-//
-//                            }
-//
-//                            @Override
-//                            public void onFailure(Call<ArrayList<UserModel>> call, Throwable t) {
-//                                Log.e("ohmygoderror",toString());
-//                            }
-//                        });
-//                    }
-//                }, 1500);
-//
-//            }
-//
-//
-//
-//        });
+        mainActivity = (MainActivity) getActivity();
+        mRecyclerView = v.findViewById(R.id.friends_recycler);
+        mLinearLayoutManager = new LinearLayoutManager(mainActivity);
+        mRecyclerView.setLayoutManager(mLinearLayoutManager);
+
+        mypic = v.findViewById(R.id.myPic);
+        SharedPreferences pref = mainActivity.getSharedPreferences("pref", MODE_PRIVATE);
+        String name = pref.getString("name", "");
+        String facebookId = pref.getString("facebookId", "");
+        String profileURL = pref.getString("profileURL", "");
+        tvname = v.findViewById(R.id.myName);
+        tvname.setText(name);
+        Glide.with(mainActivity)
+                .load(profileURL)
+                .fitCenter()
+                .into(mypic);
+
+        final AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        GraphRequest request = GraphRequest.newMeRequest(accessToken, new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+
+                new GraphRequest(
+                        accessToken,
+                        // "/me/friends",
+                        //"me/taggable_friends",
+                        "/me/friends",
+                        null,
+                        HttpMethod.GET,
+                        new GraphRequest.Callback() {
+                            public void onCompleted(GraphResponse response) {
+
+                                try {
+
+                                    JSONArray rawName = response.getJSONObject().getJSONArray("data");
+                                    Log.e("Json Array Length ", "Json Array Length " + rawName.length());
+                                    Log.e("Json Array", "Json Array " + rawName.toString());
+
+
+                                    for (int i = 0; i < rawName.length(); i++) {
+                                        JSONObject c = rawName.getJSONObject(i);
+
+
+                                        String name = c.getString("name");
+                                        Log.e("Friends's Name", "JSON NAME :" + name);
+
+                                        String id = c.getString("id");
+                                        Log.e("Friends's ID :", name + "'s ID:" + id);
+                                        if (i == rawName.length() - 1) {
+                                            friendsFacebookIds += id;
+                                        } else {
+                                            friendsFacebookIds += id + ",";
+                                        }
+
+                                    }
+                                    Log.e("friendsFacebookIds", friendsFacebookIds);
+
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                    Log.e("trycatch실패", e.toString());
+                                }
+
+                            }
+                        }
+                ).executeAsync();
+
+
+            }
+
+
+        });
+
+
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,name,profile_pic");
+        request.setParameters(parameters);
+        request.executeAsync();
+        friendsranking = new ArrayList<>();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                NetworkHelper.getInstance().FriendsRanking(friendsFacebookIds).enqueue(new Callback<ArrayList<UserModel>>() {
+                    @Override
+                    public void onResponse(Call<ArrayList<UserModel>> call, Response<ArrayList<UserModel>> response) {
+                        Log.e("rankingretrofit", response.toString());
+                        friendsranking = response.body();
+                          Log.e("How many", String.valueOf(friendsranking.size()));
+                        if (friendsranking.size() != 0) {
+                            Log.e("1st", friendsranking.get(0).getName());
+                            Log.e("1st", friendsranking.get(0).getFacebookId());
+                        }
+
+                        friendsAdapter = new FriendsAdapter(friendsranking, mainActivity);
+                        mRecyclerView.setAdapter(friendsAdapter);
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<ArrayList<UserModel>> call, Throwable t) {
+                        Log.e("ohmygoderror", toString());
+                    }
+                });
+            }
+        }, 1500);
+
+
         return v;
     }
+
 
 }
